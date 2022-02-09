e091cf6e4dc32215dd05d692dd6c3f25d7015025
Thiago <thiagowirzdemoura@gmail.com>
2022-02-09 14:11:08 -0300

    feat: DB deploy and change DB name

--- src/database.js
+++ src/database.js
@@ -4,6 +4,6 @@ dotenv.config();
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 await mongoClient.connect();
-const db = mongoClient.db("my-wallet");
+const db = mongoClient.db("driven-pizzaria");
 
 export default db;

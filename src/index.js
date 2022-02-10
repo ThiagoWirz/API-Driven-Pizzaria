969eba34e0e11892acfb19318fe2da5f9c185b0b
Vitor <vitormuller66@gmail.com>
2022-02-09 22:43:04 -0300

    feat: home requests

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 import router from "./routes/index.js";
+import db from "./database.js";
 
 const app = express();
 app.use(cors());
@@ -13,6 +14,38 @@ app.get("/", (req, res) => {
   console.log("bateu aqui");
   res.send("tudo certo bro");
 });
+
+app.post("/sales", async (req, res) => {
+  const sale = req.body
+  try {
+    await db.collection("sales").insertOne(sale);
+    res.sendStatus(201)
+
+  } catch (error) {
+    res.sendStatus(500)
+  }
+})
+
+app.delete("/sales", async (req, res) => {
+  try {
+    const sales = await db.collection("sales").findOneAndDelete({ name: 'Combo Familia' });
+    res.send(sales).status(200)
+
+  } catch (error) {
+    res.send(error).status(500)
+  }
+})
+
+app.get("/sales", async (req, res) => {
+
+  try {
+    const sales = await db.collection("sales").find({}).toArray();
+    res.send(sales).status(200)
+
+  } catch (error) {
+    res.send(error).status(500)
+  }
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Rodando na porta ${process.env.PORT}`);

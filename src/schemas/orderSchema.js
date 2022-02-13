06b6b6546fa25078b5d4d03fa08f2e8e2fd86b5d
Thiago <thiagowirzdemoura@gmail.com>
2022-02-13 13:03:48 -0300

    Add: cart at orderSchema

--- src/schemas/orderSchema.js
+++ src/schemas/orderSchema.js
@@ -4,6 +4,7 @@ const orderSchema = joi.object({
   number: joi.number().integer().required(),
   compliment: joi.string().allow(""),
   payment: joi.string().required(),
+  cart: joi.array().required(),
 });
 
 export default orderSchema;
